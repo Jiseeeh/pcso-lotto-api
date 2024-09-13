249aadbf59bf996504d5c6b5022324a8c788a24b
Jiseeeh <johncarlo.camara1@gmail.com>
2024-09-13 22:35:10 +0800

    feat: add get results today controller

--- src/routes/results.route.ts
+++ src/routes/results.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { asyncWrapper } from "./wrapper";
+import { getResultsToday } from "../controllers/results.controller";
 
 export const resultsRouter = Router();
 
-resultsRouter.get("/today")
-.get("/:date")
-.get("/today/:game_id")
+resultsRouter
+  .get("/today", asyncWrapper(getResultsToday))
+  .get("/:date")
+  .get("/today/:game_id");
