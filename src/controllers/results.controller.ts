528c21b0922bb4f620d6b2a509c7a8603d34d12d
Jiseeeh <johncarlo.camara1@gmail.com>
2024-10-29 16:52:15 +0800

    feat: re implement parse results

--- src/controllers/results.controller.ts
+++ src/controllers/results.controller.ts
@@ -1,183 +1,255 @@
 import * as cheerio from "cheerio";
-import { Request, Response, NextFunction } from "express";
+import {NextFunction, Request, Response} from "express";
 import createHttpError from "http-errors";
 
 import {
-  CORPORATIONS,
-  GAME_IDS,
-  MONTHS,
-  RESULTS_BY_DATE_URL,
-  RESULTS_TODAY_URL,
-  TARGET_KEYS,
+    CORPORATIONS,
+    DESCRIPTION_KEYS,
+    GAME_IDS,
+    MONTHS,
+    RESULTS_BY_DATE_URL,
+    RESULTS_TIME,
+    RESULTS_TODAY_URL,
 } from "../constants";
-import { GameID } from "../enum/game-id.enum";
-import { ResultTime } from "../enum/time.enum";
-import { Keys } from "../enum/keys.enum";
-import { Corporation } from "../enum/corporation.enum";
-import { ResponseBody } from "../interfaces/response-body.interface";
-import { TableData } from "../interfaces/table-data.interface";
-import { Month } from "../enum/month.enum";
-import { getDays } from "../helper/getDays";
+import {GameID} from "../enum/game-id.enum";
+import {ResultTime} from "../enum/time.enum";
+import {Keys} from "../enum/keys.enum";
+import {Corporation} from "../enum/corporation.enum";
+import {Month} from "../enum/month.enum";
+import {Game} from "../interfaces/game.interface";
+import {getDays} from "../helper/getDays";
 
 const parseResults = async (options: { url: string }) => {
-  const responseData: ResponseBody[] = [];
-  try {
-    const document = await cheerio.fromURL(options.url);
+    try {
+        const document = await cheerio.fromURL(options.url);
 
-    document(".post_content")
-      .find("figure")
-      .each((_, figure) => {
-        let gameId = document(figure)
-          .find("table thead tr th:first strong")
-          .text();
-        let corporation: string | null = document(figure)
-          .find("table thead tr th:last")
-          .text();
+        let gameId = "",
+            gameId2 = "",
+            gameId3 = "",
+            description = "",
+            corporation = "",
+            time = "",
+            result = "",
+            result2 = "",
+            result3 = "";
+        const data: Game[] = [];
 
-        if (!gameId) {
-          // ? retry to get the heading since some are not wrapped in strong.
-          gameId = document(figure).find("table thead tr th:first").text();
+        function resetValues() {
+            gameId = "";
+            gameId2 = "";
+            gameId3 = "";
+            result = "";
+            result2 = "";
+            result3 = "";
         }
 
-        if (!CORPORATIONS.includes(corporation as Corporation)) {
-          corporation = null;
-        }
+        // ? look to the respective table in the site while tracking the code (easy way)
+        document(".post_content")
+            .find("figure")
+            .each((_, figure) => {
+                const tableHeaderRowChildren = document(figure)
+                    .find("table thead tr")
+                    .children();
+                const tableBodyRowChildren = document(figure)
+                    .find("table tbody")
+                    .children();
 
-        let key = "";
-        let value = "";
-        const out = new Map<String, TableData[]>();
+                // ? loops over the table head rows to get all current games
+                // ? + the current corporation (if existing)
+                // ? + the current description (if existing)
+                for (const c of tableHeaderRowChildren) {
+                    const key = document(c).text();
 
-        if (GAME_IDS.includes(gameId as GameID)) {
-          document(figure)
-            .find("table tbody tr")
-            .each((_, tr) => {
-              document(tr)
-                .find("td")
-                .each((idx, td) => {
-                  if (idx === 0) key = document(td).text();
-                  else value = document(td).text();
-                });
+                    if (GAME_IDS.includes(key as GameID)) {
+                        if (!gameId) gameId = key;
+                        else if (!gameId2) gameId2 = key;
+                        else gameId3 = key;
+                    }
 
-              if (out.has(gameId)) {
-                let arr = out.get(gameId)!;
+                    if (CORPORATIONS.includes(key as Corporation)) {
+                        corporation = key;
+                    }
 
-                const newArr = [...arr, { time: key, winningNumbers: value }];
+                    if (DESCRIPTION_KEYS.includes(key as Keys)) {
+                        description = key;
+                    }
 
-                out.set(gameId, newArr);
-              } else {
-                out.set(gameId, [{ time: key, winningNumbers: value }]);
-              }
+                }
+
+                for (let i = 0; i < tableBodyRowChildren.length; i++) {
+                    const rowChildren = document(tableBodyRowChildren[i]).children();
+
+                    for (let j = 0; j < rowChildren.length; j++) {
+                        const key = document(rowChildren[j]).text();
+
+                        if (DESCRIPTION_KEYS.includes(key as Keys)) {
+                            description = key;
+
+                            if (gameId && gameId2) resetValues();
+                        }
+
+                        if (RESULTS_TIME.includes(key as ResultTime)) time = key;
+
+                        if (key.includes("-")) {
+                            if (!result) result = key;
+                            else if (!result2) result2 = key;
+                            else result3 = key;
+                        }
+
+                    }
+
+                    // ? we only found 1 game in the current table with a description (more likely a 6D or 6/X lotto)
+                    // ? so we reset gameId, description, and result to get the next game
+                    if (gameId && description && result && !gameId2) {
+                        data.push(<Game>{
+                            gameId,
+                            description,
+                            time: time ? time : RESULTS_TIME[RESULTS_TIME.length - 1],
+                            corporation,
+                            result,
+                        });
+
+                        if (gameId && !gameId2) {
+                            gameId = "";
+                            description = "";
+                            result = "";
+                        }
+                    }
+
+                    // ? for gameId, we reset result if we don't have a second game
+                    // ? for the server to set [result] to its respective gameId on table body loop
+                    if (gameId && time && result) {
+                        data.push({
+                            gameId,
+                            description,
+                            time,
+                            corporation,
+                            result,
+                        });
+
+                        if (!gameId2) result = "";
+
+                    }
+
+                    // ? for gameId2, we reset result if we don't have a third game
+                    // ? for the same reason as above.
+                    if (gameId2 && time && result2) {
+                        data.push({
+                            gameId: gameId2,
+                            description,
+                            time,
+                            corporation,
+                            result: result2,
+                        });
+
+
+                        if (!gameId3) {
+                            result = "";
+                            result2 = "";
+                        }
+                    }
+
+                    // ? for gameId3, we reset all results since this will be the last game
+                    if (gameId3 && time && result3) {
+                        data.push({
+                            gameId: gameId3,
+                            description,
+                            time,
+                            corporation,
+                            result: result3,
+                        });
+
+                        result = "";
+                        result2 = "";
+                        result3 = "";
+                    }
+
+                }
+                // ? reset all values every table
+                resetValues();
+                corporation = "";
             });
 
-          // ? cleanse map and retain only target keys ()
-          out.forEach((tData, key) => {
-            const newArr: TableData[] = [];
-            for (let data of tData) {
-              const dataKey = data.time as Keys | ResultTime;
+        console.table(data);
 
-              if (TARGET_KEYS.includes(dataKey)) {
-                newArr.push({
-                  ...data,
-                  // ? Replacing the 6/XX key to be at 9 since it only draws every 21:00
-                  // ? to make the key (time) more meaningful
-                  time:
-                    Keys.WINNING_COMBINATION === dataKey
-                      ? ResultTime.EVENING_9_00
-                      : data.time,
-                });
-              }
+        return data;
 
-              out.set(key, newArr);
-            }
-          });
-
-          responseData.push({
-            data: out.get(gameId)!,
-            corporation,
-            gameId,
-          });
-        }
-      });
-  } catch (error) {
-    console.error(error);
-    throw new createHttpError.NotFound(
-      "The page could not be found or the server encountered an error while parsing."
-    );
-  }
-
-  return responseData;
+    } catch (error) {
+        throw new createHttpError.InternalServerError(
+            "The server encountered an error while parsing the results."
+        );
+    }
 };
 
 export const getResultsToday = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  const responseData = await parseResults({
-    url: RESULTS_TODAY_URL,
-  });
+    const responseData = await parseResults({
+        url: RESULTS_TODAY_URL,
+    });
 
-  res.status(200).send(responseData);
+    res.status(200).send(responseData);
 };
 
 export const getResultsByDate = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  // ? 3-9 letters month
-  // ? 1-2 digits days
-  // ? 4 digits year
-  const dateRegex = /\w{3,9}-\d{1,2}-\d{4}/g;
-  const { date } = req.params;
+    // ? 3-9 letters month
+    // ? 1-2 digits days
+    // ? 4 digits year
+    const dateRegex = /\w{3,9}-\d{1,2}-\d{4}/g;
+    const {date} = req.params;
 
-  if (!date.match(dateRegex)) {
-    next(
-      new createHttpError.BadRequest(
-        "Please adhere to the proper format, see at <link>"
-      )
-    );
-    return;
-  }
+    if (!date.match(dateRegex)) {
+        next(
+            new createHttpError.BadRequest(
+                "Please adhere to the proper format, see at <link>"
+            )
+        );
+        return;
+    }
 
-  // ? double check date
-  // ? start of records from site =  aug 26, 2020 - present
-  // ? start date that can be scraped with current impl = june 3 2024
-  const [month, day, year] = date.split("-");
+    // ? double check date
+    // ? start of records from site =  aug 26, 2020 - present
+    const [month, day, year] = date.split("-");
 
-  if (!MONTHS.includes(month.toLowerCase() as Month)) {
-    next(new createHttpError.BadRequest("Please send a valid month."));
-    return;
-  }
+    if (!MONTHS.includes(month.toLowerCase() as Month)) {
+        next(new createHttpError.BadRequest("Please send a valid month."));
+        return;
+    }
 
-  const monthDays = getDays(parseInt(year), MONTHS.indexOf(month as Month));
-  const validYear = 2020;
+    const monthDays = getDays(parseInt(year), MONTHS.indexOf(month as Month));
+    const validYear = 2020;
 
-  if (parseInt(day) > monthDays || parseInt(day) <= 0) {
-    next(
-      new createHttpError.BadRequest(
-        `Days of ${`${month.charAt(0).toUpperCase()}${month
-          .slice(1, month.length)
-          .toLowerCase()}`} are only ${monthDays}.`
-      )
-    );
-    return;
-  }
+    if (parseInt(day) > monthDays || parseInt(day) <= 0) {
+        next(
+            new createHttpError.BadRequest(
+                `Days of ${`${month.charAt(0).toUpperCase()}${month
+                    .slice(1, month.length)
+                    .toLowerCase()}`} are only ${monthDays}.`
+            )
+        );
+        return;
+    }
 
-  if (parseInt(year) < validYear) {
-    next(
-      new createHttpError.BadRequest(
-        `Years earlier than ${validYear} is not supported.`
-      )
-    );
-    return;
-  }
+    if (parseInt(year) < validYear) {
+        next(
+            new createHttpError.BadRequest(
+                `Years earlier than ${validYear} is not supported.`
+            )
+        );
+        return;
+    }
 
-  const url = RESULTS_BY_DATE_URL + date;
+    const url = RESULTS_BY_DATE_URL + date;
 
-  const responseData = await parseResults({
-    url,
-  });
+    const responseData = await parseResults({
+        url,
+    });
 
-  res.status(200).send(responseData);
+    res.status(200).send(responseData);
 };
