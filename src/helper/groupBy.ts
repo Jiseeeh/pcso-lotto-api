4c755bb09a09e5cb08a6742b2393c178bdafd946
Jiseeeh <johncarlo.camara1@gmail.com>
2024-11-15 23:20:16 +0800

    fix: redundant gameId showing on results

--- src/helper/groupBy.ts
+++ src/helper/groupBy.ts
@@ -1,6 +1,8 @@
 export const groupBy = function (items: any[], key: string) {
     return items.reduce(function (prev, current) {
-        (prev[current[key]] = prev[current[key]] || []).push(current);
+        // ? removes gameId
+        let {gameId, ...newCurrent} = current;
+        (prev[current[key]] = prev[current[key]] || []).push(newCurrent);
         return prev;
     }, {});
 };
