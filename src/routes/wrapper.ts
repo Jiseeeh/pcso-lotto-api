c1f4cc9d9801e7e37cdacb1f8f38674104e87507
Jiseeeh <johncarlo.camara1@gmail.com>
2025-02-08 22:34:25 +0800

    chore: add docs

--- src/routes/wrapper.ts
+++ src/routes/wrapper.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
+// TODO: remove when express gets updated to 5+
 export function asyncWrapper(cb: Function) {
 	return async function (req: Request, res: Response, next: NextFunction) {
 		try {
